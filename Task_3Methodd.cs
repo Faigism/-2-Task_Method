2a9651a58f9109e3ff3e03a5f43557791343ede5
Faigism <121167870+Faigism@users.noreply.github.com>
2023-01-18 12:20:46 +0400

    Add files via upload

---
+++ Task_3Methodd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Task_1metod
+{
+    internal class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            var result = Sums(2412);
+            Console.WriteLine(result);
+        }
+        static int Sums(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                var lastdigit = n % 10;
+                sum += lastdigit;
+                n = (n - lastdigit) / 10;
+            }
+            return sum;
+        }
+        
+        
+
+    }
+}
