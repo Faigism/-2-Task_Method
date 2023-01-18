f9d5628239280c431efd3326f1d59ac6be20ce3b
Faigism <121167870+Faigism@users.noreply.github.com>
2023-01-18 12:49:59 +0400

    Add files via upload

---
+++ Task_3Metoddd.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Task_3Metod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Char("Hello");
+            Console.WriteLine(result);
+        }
+        static bool Char(string n)
+        {
+            bool has_wanted_char = false;
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] == 'a')
+                {
+                    has_wanted_char = true;
+                    break;
+                }return true;
+            }
+            if (has_wanted_char == false)
+            {
+                return false;
+            }
+            
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
