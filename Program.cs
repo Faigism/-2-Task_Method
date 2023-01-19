9247aadbeac04281dd9eca4ebba022158637e7b4
Faigism <121167870+Faigism@users.noreply.github.com>
2023-01-19 12:11:46 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace _3_.Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilmiş yazını əksinə çevirən metod (misalçün metoda "salam" göndərsəniz metod geriyə "malas" return etməlidir)
+
+            Show("S", "a", "l", "a", "m");
+        }
+        static void Show(string str1,string str2,string str3,string str4,string str5)
+        {
+            Console.WriteLine(str5+str4+str3+str2+str1);
+        }
+
+    }
+}
---
+++ Program123.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //-Verilmiş yazıda rəqəm olub olmadığını tapan metod?
+            var result = Numb("fascx");
+            Console.WriteLine(result);
+        }
+        static bool Numb(string n)
+        {
+            bool has_Wanted_Int = false;
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] == '0' || n[i] == '1' || n[i] == '2' || n[i] == '3' || n[i] == '4' || n[i] == '5' || n[i] == 6 || n[i] == '7' || n[i] == '8' || n[i] == '9')
+                {
+                    return true;
+                }
+            }
+            return has_Wanted_Int;
+            
+        }
+    }
+}
