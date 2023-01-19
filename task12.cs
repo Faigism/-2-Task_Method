35a89c9c50b94c089d7764e660bc38e59ccb437b
Faigism <121167870+Faigism@users.noreply.github.com>
2023-01-19 12:15:12 +0400

    Add files via upload

---
+++ task12.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //-Verilmiş ədədlər siyahısındaki ədədlərdən ən kiçiyini tapan metod?
+            Smallnum();
+        }
+        static void Smallnum()
+        {
+            int[] n = { 2, 30, 1, 3, 34, 12, 50 };
+            Console.WriteLine(n.Min());
+        }
+        
+        
+
+    }
+}
